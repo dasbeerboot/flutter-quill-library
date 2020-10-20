89f6c462c2f6a9fa9e6d2297c17fd14a17968040
Juwon Chun <juwonchun@gmail.com>
2020-10-20 18:58:51 +0900

    Add button for JS communication

--- flutter_web_study/lib/web_view.dart
+++ flutter_web_study/lib/web_view.dart
@@ -1,206 +1,66 @@
 import 'dart:async';
-
 import 'package:easy_web_view/easy_web_view.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter/material.dart';
+import 'get_html_button.dart';
 
 
 class HomeScreen extends StatelessWidget {
-
   // final Completer<WebViewController> _controller = Completer<WebViewController>();
   WebViewController _controller;
-
+  // WebViewController webViewController;
 
   @override
   Widget build(BuildContext context) {
+    String quillHTML = '';
     return Scaffold(
       appBar: AppBar(
         title: Text('QUILL'),
       ),
-      body: Container(
-        child: WebView(
-          initialUrl: "https://dasbeerboot.github.io/wysiwyg-editor-examples/subpages/quill.html",
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (WebViewController webViewController) {
-            String quillHTML = '';
-            webViewController.evaluateJavascript(
-              "function test () {return document.getElementById('result-area')} test()"
-            ).then((result) => quillHTML = result);
-            print("result ???? " + quillHTML);
-          },
-        ),
-      )
+      body: Stack(
+        children: <Widget>[
+          WebView(
+            initialUrl: "https://dasbeerboot.github.io/wysiwyg-editor-examples/subpages/quill.html",
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (WebViewController webViewController) {
+              _controller = webViewController;
+
+              // webViewController.evaluateJavascript(
+              //   "window.getQuillHTML.postMessage(window.document)"
+              // );
+            },
+            javascriptChannels: <JavascriptChannel>[
+              JavascriptChannel(
+                name: 'getQuillHTML', 
+                onMessageReceived: (JavascriptMessage result) {
+                  quillHTML = result.message.toString();
+                  print(quillHTML);
+                }
+                ),
+                JavascriptChannel(
+                  name: 'testEvaluateJS'
+                , onMessageReceived: (JavascriptMessage result) {
+                  print("test test test : " + result.message.toString());
+                })
+            ].toSet(),
+          ),
+
+          Container(
+            child: Positioned(
+              bottom: 10,
+              right: 10,
+              child: FlatButton(
+                color: Colors.blue,
+                onPressed: (){
+                  _controller.evaluateJavascript(
+                    "window.getQuillHTML()"
+                  ).then((result) => print("RESULT !!!!! " + result.toString()));
+                }, 
+                child: Text("GET HTML"))
+                )
+          )
+        ]
+      ),
     );
   }
 }
-
-// class HomeScreen extends StatefulWidget {
-//   @override
-//   _HomeScreenState createState() => _HomeScreenState();
-// }
-
-// class _HomeScreenState extends State<HomeScreen> {
-//   String src = 'https://dasbeerboot.github.io/wysiwyg-editor-examples/subpages/quill.html';
-//   static ValueKey key = ValueKey('key_0');
-//   static ValueKey key2 = ValueKey('key_1');
-//   static ValueKey key3 = ValueKey('key_2');
-//   bool _isHtml = false;
-//   bool _isMarkdown = false;
-//   bool _useWidgets = false;
-//   bool _editing = false;
-//   bool _isSelectable = false;
-
-//   bool open = false;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: Text('Quill editor example'),
-//           leading: IconButton(
-//             icon: Icon(Icons.access_time),
-//             onPressed: () {
-//               setState(() {
-//                 print("Click!");
-//                 open = !open;
-//               });
-//             },
-
-//             //tooltip: "Menu",
-//           ),
-//           actions: <Widget>[
-//             Builder(builder: (context) {
-//               return IconButton(
-//                 icon: Icon(_editing ? Icons.close : Icons.settings),
-//                 onPressed: () {
-//                   if (mounted)
-//                     setState(() {
-//                       _editing = !_editing;
-//                     });
-//                 },
-//               );
-//             }),
-//           ],
-//         ),
-//         body: _editing
-//             ? SingleChildScrollView(
-//                 child: Column(
-//                   children: <Widget>[
-//                     SwitchListTile(
-//                       title: Text('Html Content'),
-//                       value: _isHtml,
-//                       onChanged: (val) {
-//                         if (mounted)
-//                           setState(() {
-//                             _isHtml = val;
-//                             if (val) {
-//                               _isMarkdown = false;
-//                               src = htmlContent;
-//                             } else {
-//                               src = url;
-//                             }
-//                           });
-//                       },
-//                     ),
-//                     SwitchListTile(
-//                       title: Text('Markdown Content'),
-//                       value: _isMarkdown,
-//                       onChanged: (val) {
-//                         if (mounted)
-//                           setState(() {
-//                             _isMarkdown = val;
-//                             if (val) {
-//                               _isHtml = false;
-//                               src = markdownContent;
-//                             } else {
-//                               src = url;
-//                             }
-//                           });
-//                       },
-//                     ),
-//                     SwitchListTile(
-//                       title: Text('Use Widgets'),
-//                       value: _useWidgets,
-//                       onChanged: (val) {
-//                         if (mounted)
-//                           setState(() {
-//                             _useWidgets = val;
-//                           });
-//                       },
-//                     ),
-//                     SwitchListTile(
-//                       title: Text('Selectable Text'),
-//                       value: _isSelectable,
-//                       onChanged: (val) {
-//                         if (mounted)
-//                           setState(() {
-//                             _isSelectable = val;
-//                           });
-//                       },
-//                     ),
-//                   ],
-//                 ),
-//               )
-//             : Stack(
-//                 children: <Widget>[
-//                   Row(
-//                     children: <Widget>[
-//                       Expanded(
-//                           flex: 1,
-//                           child: EasyWebView(
-//                               src: src,
-//                               onLoaded: () {
-//                                 print('$key: Loaded: $src');
-//                               },
-//                               isHtml: _isHtml,
-//                               isMarkdown: _isMarkdown,
-//                               convertToWidgets: _useWidgets,
-//                               key: key
-//                               // width: 100,
-//                               // height: 100,
-//                               )),
-//                     ],
-//                   ),
-//                   Column(
-//                     children: <Widget>[
-//                       Expanded(
-//                         flex: 3,
-//                         child: Container(),
-//                       ),
-//                     ],
-//                   )
-//                 ],
-//               ));
-//   }
-
-//   String get htmlContent => """
-// <!DOCTYPE html>
-// <html>
-// <head>
-// <title>Quill Editor</title>
-// </head>
-// <body>
-// <h1>This is a Heading</h1>
-// <p>This is a paragraph.</p>
-// </body>
-// </html>
-// """;
-
-//   String get markdownContent => """
-// # This is a heading
-// ## Here's a smaller heading
-// This is a paragraph
-// * Here's a bulleted list
-// * Another item
-// 1. And an ordered list
-// 1. The numbers don't matter
-// > This is a qoute
-// [This is a link to Flutter](https://flutter.dev)
-// """;
-
-//   String get embeedHtml => """
-// <iframe width="560" height="315" src="https://www.youtube.com/embed/rtBkU4pvHcw?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-// """;
-
-//   String get url => 'https://flutter.dev';
-// }
